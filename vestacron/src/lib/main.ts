5eb6483ea4d36ad01904d43e55822277e098db9f
Olivier Huin <olih@users.noreply.github.com>
2020-11-18 21:35:36 +0000

    hardcode crom example

--- vestacron/src/lib/main.ts
+++ vestacron/src/lib/main.ts
@@ -1,5 +1,5 @@
-import { loadLocalSchedule } from './service/schedule-loader';
-import { publishToTopic } from './service/schedule-publisher';
+// import { loadLocalSchedule } from './service/schedule-loader';
+import { publishSchedule } from './service/schedule-publisher';
 
 interface ServiceResponse {
   readonly body: string;
@@ -21,10 +21,62 @@ const createResponse = (body: string) => ({
   statusCode: 200,
 })
 
+const exSchedule = {
+  "date": "2020-11-01",
+  "date_human": "Sunday, 01 November",
+  "weekday": 7,
+  "events": [
+    {
+      "id": "christmas",
+      "description": "Christmas",
+      "flags": "event"
+    }
+  ],
+  "casual_tasks": [
+    {
+      "id": "brush-teeth",
+      "description": "Brush teeth",
+      "flags": "Daily"
+    }
+  ],
+  "occasional_tasks": [
+    {
+      "id": "clean-windows",
+      "description": "Clean windows",
+      "flags": "Quaterly"
+    }
+  ],
+  "shopping": [
+    {
+      "id": "bread",
+      "description": "Bread",
+      "flags": "Weekly"
+    },
+    {
+      "id": "butter",
+      "description": "Butter",
+      "flags": "Weekly"
+    }
+  ],
+  "lunch": {
+    "id": "ratatouille",
+    "description": "Ratatouille",
+    "link": "https://website.com/ratatouille",
+    "flags": "fresh"
+  },
+  "supper": {
+    "id": "pasta",
+    "description": "Pasta",
+    "link": "https://website.com/pasta",
+    "flags": "fresh x2"
+  }
+}
+
 const handler: ServiceHandler = async (event: ServiceEvent) => {
   try {
-    const schedule = await loadLocalSchedule(event.time)
-    await publishToTopic(schedule)
+    //const schedule = await loadLocalSchedule(event.time)
+    const schedule = exSchedule
+    await publishSchedule(schedule)
     return createResponse(`Schedule Daily update ${event.time} on ${schedule.date_human}`)
   } catch (error) {
     console.error(error)
