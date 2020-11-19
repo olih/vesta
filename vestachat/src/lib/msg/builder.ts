2a6a65664c484e4a6aaa219f901278df61005ccc
Olivier Huin <olih@users.noreply.github.com>
2020-11-19 21:38:36 +0000

    Deal with zero and plus ten limits

--- vestachat/src/lib/msg/builder.ts
+++ vestachat/src/lib/msg/builder.ts
@@ -5,6 +5,16 @@ import {
   createPlainHeader,
 } from './slackmsg';
 
+const chunk = (items: any[], size: number) => {
+  const chunked_arr = [];
+  let copied = [...items];
+  const numOfChild = Math.ceil(copied.length / size);
+  for (let i = 0; i < numOfChild; i++) {
+    chunked_arr.push(copied.splice(0, size));
+  }
+  return chunked_arr;
+};
+
 const fromDailyMessage = (msg: DailyMessage) => {
   const blocks = [
     createPlainHeader(msg.title),
@@ -12,16 +22,24 @@ const fromDailyMessage = (msg: DailyMessage) => {
     createMarkdownSection(msg.supper),
   ];
   if (msg.events.length > 0) {
-    blocks.push(createCheckboxes('Events', msg.events));
+    chunk(msg.events, 10).forEach(group =>
+      blocks.push(createCheckboxes('Events', group))
+    );
   }
   if (msg.casual_tasks.length > 0) {
-    blocks.push(createCheckboxes('Casual tasks', msg.casual_tasks));
+    chunk(msg.casual_tasks, 10).forEach(group =>
+      blocks.push(createCheckboxes('Casual tasks', group))
+    );
   }
   if (msg.occasional_tasks.length > 0) {
-    blocks.push(createCheckboxes('Occasional tasks', msg.occasional_tasks));
+    chunk(msg.occasional_tasks, 10).forEach(group =>
+      blocks.push(createCheckboxes('Occasional tasks', group))
+    );
   }
   if (msg.shopping.length > 0) {
-    blocks.push(createCheckboxes('Shopping', msg.shopping));
+    chunk(msg.shopping, 10).forEach(group =>
+      blocks.push(createCheckboxes('Shopping', group))
+    );
   }
   return blocks;
 };
--- vestachat/test/daily.test.ts
+++ vestachat/test/daily.test.ts
@@ -266,3 +266,214 @@ test('Convert from schedule with empty parts', () => {
   expect(dailyMsg).toEqual(expectedMsg);
   expect(blocks).toEqual(expectedBlocks);
 });
+
+test('Convert from schedule with  more than 10 items', () => {
+  const createDailyTask = (id: number) => createTask(id, 'Daily');
+  const schedule: Schedule = {
+    date: '2011-11-10',
+    date_human: 'Nov 10',
+    casual_tasks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(createDailyTask),
+    occasional_tasks: [],
+    shopping: [],
+    events: [],
+    lunch: createRecipe(10, ''),
+    supper: createRecipe(11, ''),
+  };
+  const expectedMsg: DailyMessage = {
+    title: 'Today Nov 10',
+    lunch: '*Lunch*: Recipe 10\n<http://website.com/10|lunch recipe>',
+    supper: '*Supper*: Recipe 11\n<http://website.com/11|supper recipe>',
+    casual_tasks: [
+      {
+        title: 'description 1 _Daily_',
+        value: 'id-1',
+      },
+      {
+        title: 'description 2 _Daily_',
+        value: 'id-2',
+      },
+      {
+        title: 'description 3 _Daily_',
+        value: 'id-3',
+      },
+      {
+        title: 'description 4 _Daily_',
+        value: 'id-4',
+      },
+      {
+        title: 'description 5 _Daily_',
+        value: 'id-5',
+      },
+      {
+        title: 'description 6 _Daily_',
+        value: 'id-6',
+      },
+      {
+        title: 'description 7 _Daily_',
+        value: 'id-7',
+      },
+      {
+        title: 'description 8 _Daily_',
+        value: 'id-8',
+      },
+      {
+        title: 'description 9 _Daily_',
+        value: 'id-9',
+      },
+      {
+        title: 'description 10 _Daily_',
+        value: 'id-10',
+      },
+      {
+        title: 'description 11 _Daily_',
+        value: 'id-11',
+      },
+      {
+        title: 'description 12 _Daily_',
+        value: 'id-12',
+      },
+    ],
+    occasional_tasks: [],
+    shopping: [],
+    events: [],
+  };
+
+  const expectedBlocks = [
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: 'Today Nov 10',
+        emoji: false,
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*Lunch*: Recipe 10\n<http://website.com/10|lunch recipe>',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: '*Supper*: Recipe 11\n<http://website.com/11|supper recipe>',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'Casual tasks',
+      },
+      accessory: {
+        type: 'checkboxes',
+        options: [
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 1 _Daily_',
+            },
+            value: 'id-1',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 2 _Daily_',
+            },
+            value: 'id-2',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 3 _Daily_',
+            },
+            value: 'id-3',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 4 _Daily_',
+            },
+            value: 'id-4',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 5 _Daily_',
+            },
+            value: 'id-5',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 6 _Daily_',
+            },
+            value: 'id-6',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 7 _Daily_',
+            },
+            value: 'id-7',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 8 _Daily_',
+            },
+            value: 'id-8',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 9 _Daily_',
+            },
+            value: 'id-9',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 10 _Daily_',
+            },
+            value: 'id-10',
+          },
+        ],
+        action_id: 'checkboxes-action',
+      },
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'Casual tasks',
+      },
+      accessory: {
+        type: 'checkboxes',
+        options: [
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 11 _Daily_',
+            },
+            value: 'id-11',
+          },
+          {
+            text: {
+              type: 'mrkdwn',
+              text: 'description 12 _Daily_',
+            },
+            value: 'id-12',
+          },
+        ],
+        action_id: 'checkboxes-action',
+      },
+    },
+  ];
+  const dailyMsg = fromSchedule(schedule);
+  const blocks = fromDailyMessage(dailyMsg);
+  expect(dailyMsg).toEqual(expectedMsg);
+  expect(blocks).toEqual(expectedBlocks);
+});
